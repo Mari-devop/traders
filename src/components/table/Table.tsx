bf6ee1ebb0cc51f4d42ca8a6810a561f45034c39
Mari-devop <mari.philon@gmail.com>
2024-08-06 14:55:29 +0300

    fix warn

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Table, TableBody, TableRow, TableHead, Pagination } from '@mui/material';
 import {
   TableContainerStyled, TableContainer, Header, TableHeader, FirstTableHeader, PaginationContainer,
